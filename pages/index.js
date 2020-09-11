ddb32e6ba9a0c50d7d7e06cd3d5bbc21cc17e493
Ryan Madsen <ryan.madsen@gmail.com>
2020-09-10 20:29:17 -0700

    use antd styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Formulate from "formulate-core";
+import renderer from "formulate-antd";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { render } from "react-dom";
 
 const schema = {
   fields: [
@@ -20,7 +22,7 @@ export default function Home() {
         <h1 className={styles.title}>Thank you for visiting!</h1>
 
         <p className={styles.description}>We would love your feedback...</p>
-        <Formulate schema={schema} />
+        <Formulate schema={schema} onSubmit={console.log} renderer={renderer} />
       </main>
 
       <footer className={styles.footer}>Powered by Formulate</footer>
